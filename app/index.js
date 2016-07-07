68a5918babcf6ee386256bddbcfcb39ccf4607cd
Michael Dixon <qualitydixon@gmail.com>
2016-07-07 15:55:57 -0600

    add redux thunk

--- app/index.js
+++ app/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import * as reducers from './modules'
 import List from './components/List'
 
-let store = createStore(combineReducers({...reducers}), window.devToolsExtension && window.devToolsExtension())
+let store = createStore(combineReducers({...reducers}), compose(
+              applyMiddleware(thunk),
+              window.devToolsExtension ? window.devToolsExtension() : f => f
+            ))
+
+
 
 ReactDOM.render(<Provider store={store}>
                   <List />
